368c376a618c923f1434fc5e5e1d224ce3629b21
option0417 <option0417@gmail.com>
2013-02-28 00:02:05 +0800

    Refactor For EclipsePlatform

--- main.cpp
+++ main.cpp
@@ -1,29 +1,26 @@
 #include <cstdlib>
 #include <iostream>
 #include "src/core/Core.h"
-#include "gtest/gtest.h"
 #include "src/domain/Board.h"
 #include "src/ai/AlphaBeta.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
-	//testing::InitGoogleTest(&argc, argv);
-    //RUN_ALL_TESTS();
 
-//	Core *core = new Core();
-//	core->startGame();
-//	delete core;
+	Core *core = new Core();
+	core->startGame();
+	delete core;
 
-	Board* board = new Board();
-	board->initial();
-
-	AlphaBeta *ab = AlphaBeta::getInstance(board);
-
-	int depth = -1;
-	cout<<"Depth : ";
-	cin>>depth;
-	cout<<"Value : "<<ab->runAlgorithm(-1000, 1000, depth, true);
+//	Board* board = new Board();
+//	board->initial();
+//
+//	AlphaBeta *ab = AlphaBeta::getInstance(board);
+//
+//	int depth = -1;
+//	cout<<"Depth : ";
+//	cin>>depth;
+//	cout<<"Value : "<<ab->runAlgorithm(-1000, 1000, depth, true);
 
 	return 0;
 }
